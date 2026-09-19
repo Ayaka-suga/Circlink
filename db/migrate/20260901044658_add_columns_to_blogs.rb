44586a62e10779b017e183c1aaa905a939109a24
Ayaka-suga <24tb7095@student.gs.chiba-u.jp>
2026-09-19 17:45:00 +0900

    Fix duplicate blog migration

--- db/migrate/20260901044658_add_columns_to_blogs.rb
+++ db/migrate/20260901044658_add_columns_to_blogs.rb
@@ -1,7 +1,5 @@
 class AddColumnsToBlogs < ActiveRecord::Migration[7.2]
   def change
-    add_column :blogs, :title, :string
-    add_column :blogs, :content, :text
-    add_column :blogs, :start_time, :datetime
+    
   end
 end
